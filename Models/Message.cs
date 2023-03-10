01bb3cc1f54b7ea3caa0a1ab1c1fb04544efe289
Aly Mamdouh <Aly.Mamdouh@InteractTS.com>
2023-03-10 15:03:36 +0200

    set message time automatic

--- Models/Message.cs
+++ Models/Message.cs
@@ -8,12 +8,17 @@ namespace ChatApp.Models
 {
     public class Message
     {
+        public Message()
+        {
+            When = DateTime.Now;
+        }
+
         public int Id { get; set; }
         [Required]
         public string UserName { get; set; }
         [Required]
         public string Text { get; set; }
-        public DateTime When { get; set; }
+        public DateTime When { get; set; }= DateTime.Now;
         public string UserId { get; set; }
         public virtual AppUser Sender { get; set; }
     }
